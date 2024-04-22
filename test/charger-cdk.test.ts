1234792672c98c97e3da57cc3da60932308b9768
Dan Seagren <seagrendaniel562@gmail.com>
2024-04-21 20:23:34 -0700

    Initial commit

---
+++ test/charger-cdk.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as ChargerCdk from '../lib/charger-cdk-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/charger-cdk-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new ChargerCdk.ChargerCdkStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
